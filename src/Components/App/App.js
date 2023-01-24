7e039a9b6e7581458e396df398cdbe0d5e8832f5
Vitor Carvalho <vitor.velozo2003@gmail.com>
2023-01-24 02:13:01 -0300

    Add spotify

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,7 @@ import React from 'react';
     this.removeTrack = this.removeTrack.bind(this)
     this.updatePlaylistName = this.updatePlaylistName.bind(this)
     this.savePlaylist = this.savePlaylist.bind(this)
+    this.search = this.search.bind(this)
   }
 
   updatePlaylistName(name) {
@@ -49,12 +50,18 @@ import React from 'react';
     const trackUris = this.state.playlistTracks.map(track => track.uri)
   }
 
+  search(term) {
+    console.log(term)
+
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar
+            onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults 
               searchResults={this.state.searchResults} 
